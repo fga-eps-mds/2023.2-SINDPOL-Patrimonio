c7859d040b119f8b5035472af6c7ceaa66845f4e
Matheus Afonso <matheusafonsouza@gmail.com>
2023-10-22 01:39:22 -0300

    chore: remove dummy

--- patrimonio/web/api/router.py
+++ patrimonio/web/api/router.py
@@ -1,8 +1,7 @@
 from fastapi.routing import APIRouter
 
-from patrimonio.web.api import dummy, echo, monitoring
+from patrimonio.web.api import echo, monitoring
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
-api_router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
